a39fc958efe9166e366a2f1b9f22e00bbc39768c
Sergio <sergio_basket93@hotmail.com>
2016-11-23 12:39:30 +0100

    Añadida funcionalida base de datos

--- Server.java
+++ Server.java
@@ -9,10 +9,10 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) {
-		int port = 8989;
+		int port = 8080;
 		ServerSocket serverSocket = null;
 		Socket socket = null;
-		initializeDB();
+
 		try {
 			serverSocket = new ServerSocket(port);
 			do {
@@ -22,19 +22,6 @@ public class Server {
 			} while (true);
 		} catch (IOException e) {
 			System.err.println("Error de entrada/salida al abrir el socket.");
-		}
-	}
-
-	static void initializeDB(){
-		String fileName = "database.txt";
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-			for (int i=0;i<10;i++) writer.write(i+" 0\n");
-			writer.close();         
-		} catch(FileNotFoundException ex) {
-			System.out.println("Unable to open file '" + fileName + "'");                
-		} catch(IOException ex) {
-			ex.printStackTrace();
 		}
 	}
 
